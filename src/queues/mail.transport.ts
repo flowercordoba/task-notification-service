109fe30ba48f50de06be2a5b5d75c17285d38cd1
flowercordoba <flowercordoba7@gmail.com>
2024-01-19 23:52:09 -0500

    test upadate

--- src/queues/mail.transport.ts
+++ src/queues/mail.transport.ts
@@ -1,6 +1,6 @@
+import { IEmailLocals, winstonLogger } from '@flowercordoba/task-shared-library';
 import { config } from '@notifications/config';
 import { emailTemplates } from '@notifications/helpers';
-import { IEmailLocals, winstonLogger } from '@uzochukwueddie/jobber-shared';
 import { Logger } from 'winston';
 
 const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'mailTransport', 'debug');
