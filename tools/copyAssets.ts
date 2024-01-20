a5577135f7df1f87ce209838c9ac2ee3c1df8451
flowercordoba <flowercordoba7@gmail.com>
2024-01-20 00:25:00 -0500

    added docker files update

--- tools/copyAssets.ts
+++ tools/copyAssets.ts
@@ -1,3 +1,8 @@
 import * as shell from 'shelljs';
+// import fs from 'fs-extra';
+// import * as fse from 'fs-extra';
 
 shell.cp('-R', 'src/emails', 'build/src/');
+
+// fs.copySync('src/emails', 'build/src/emails');
+// fse.copySync('src/emails', 'build/src/emails');
